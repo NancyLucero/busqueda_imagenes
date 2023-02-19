f6bf58a81503d16ed62b23f267dfed3ee0e14a80
Nancy Lucero <nancylucero23@gmail.com>
2023-02-19 12:00:55 -0300

    feat: se instalo api axios

--- NancyLucero-B/src/App.jsx
+++ NancyLucero-B/src/App.jsx
@@ -1,19 +1,33 @@
 import React, { useState, useEffect } from 'react'
+import	axios from 'axios'
 
 import './App.css'
 
-function App() {
-  const [valor,setValor]= useState('');
-  const [res, setRes] = useState([]);
+const App = () =>{
 
+  const [inputvalor,setInputvalor]= useState('');  
+  const [imagenes, setImagenes] = useState([]);
+  const API_KEY='7mdmq8E6PgLgqdaAXeKm2EGvkwE05mu3-WyxWJb2WEM';
+  const URL =`https://api.unsplash.com/search/photos?page=1&query=${inputvalor}&client_id=${API_KEY}`;
+
+ useEffect(()=>{
+    const BuscarImagenes = async()=>{
+      const response = await axios.get(URL)
+      setImagenes(response.data.results)
+    }
+    BuscarImagenes()
+ })
+
+ /*
   const BuscarResultados = async ()=>{
     const API_KEY='7mdmq8E6PgLgqdaAXeKm2EGvkwE05mu3-WyxWJb2WEM';
-    const URL =`https://api.unsplash.com/photos/?client_id=${API_KEY}&query=${valor}`;
+    const URL =`https://api.unsplash.com/search/photos?page=1&query=${inputvalor}&client_id=${API_KEY}`;
+    const URL =`https://api.unsplash.com/search/photos?page=1&query=coding&client_id=${API_KEY}`;
     const response= await fetch(URL);
     const data= await response.json();
     setRes(data.results);
     console.log(data);
-  }
+  }*/
 
   const entradas= document.querySelectorAll('input');
   entradas.forEach( entrada =>{
@@ -37,7 +51,7 @@ function App() {
       <div>
         <label className='label'>
           <span>buscar imagenes...</span>
-          <input type="text" autoComplete='off' onChange={(e) => setValor(e.target.value)}/>
+          <input type="text" autoComplete='off' onChange={(e) => setInputvalor(e.target.value)}/>
         </label>
       </div>
       <div>
@@ -47,17 +61,16 @@ function App() {
 
       <div className='container'>
           <div className='grilla'>
-          {
-            res.map((elemento,indice) =>{
-              return(
-                <img key={indice} src={elemento.urls.regular} alt="" />
-              )                          
-          })          
-        }
-        </div>
+          {imagenes.map(imagen=>(
+              <div key={imagen.id}>            
+                <img src={imagen.urls.regular} alt="" />
+              </div>
+            ))
+          }
+          </div>
       </div>
     </div>
   )
-}
+  }
 
 export default App
