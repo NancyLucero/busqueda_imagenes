6361a3ae6704c15bad6745aa8e15ec7eb5f8c77b
Nancy Lucero <nancylucero23@gmail.com>
2023-02-26 13:11:30 -0300

    feat: se agrego el complemento InfinitScroll

--- NancyLucero-B/src/App.jsx
+++ NancyLucero-B/src/App.jsx
@@ -1,23 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import	axios from 'axios'
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import InfiniteScroll from "react-infinite-scroll-component";
 import './App.css'
 
 const App = () =>{
 
   const [inputvalor,setInputvalor]= useState('');  
   const [imagenes, setImagenes] = useState([]);
-  const API_KEY='7mdmq8E6PgLgqdaAXeKm2EGvkwE05mu3-WyxWJb2WEM';
-  const URL =`https://api.unsplash.com/search/photos?page=1&query=${inputvalor}&client_id=${API_KEY}`;
+  const [page, setPage] = useState(0);
 
- useEffect(()=>{
-    const BuscarImagenes = async()=>{
-      const response = await axios.get(URL)
-      setImagenes(response.data.results)
-    }
-    BuscarImagenes()
- })
+  const API_KEY='7mdmq8E6PgLgqdaAXeKm2EGvkwE05mu3-WyxWJb2WEM';
+  /*const URL =`https://api.unsplash.com/search/photos?page=1&per_page=20&query=${inputvalor}&client_id=${API_KEY}`;  */
+
+  
+
+  const BuscarImagenes = async(nropage,busqueda)=>{
+    const URL =`https://api.unsplash.com/search/photos?page=${nropage}&per_page=20&query=${busqueda}&client_id=${API_KEY}`;
+    const response = await axios.get(URL)
+    setImagenes(response.data.results)
+    let aux = imagenes.concat(response.data.results)
+    setImagenes(aux)
+  }
+  
+  useEffect(()=>{
+      setTimeout(()=>{
+        BuscarImagenes(page+1,inputvalor)
+      },100)
+  },[])
 
  /*
   const BuscarResultados = async ()=>{
@@ -30,7 +40,10 @@ const App = () =>{
     console.log(data);
   }*/
 
-  const estilocard = "with: 18rem;";
+  const estilocard = {
+    with: '18rem',
+    overflow:'auto'
+  }; 
 
   const entradas= document.querySelectorAll('input');
   entradas.forEach( entrada =>{
@@ -62,34 +75,51 @@ const App = () =>{
   </div>*/}
       </form>
    
-      <div className='container fondo'>
+      <div className='container fondo'id='ScrollInfinito'>  
+        <InfiniteScroll
+          dataLength={imagenes.length}
+          next={()=>{
+             BuscarImagenes(page+1,inputvalor);
+          }}
+          hasMore={true}
+          loader={<h4>Esperar .....</h4>}
+          scrollableTarget="ScrollInfinito"
+          >
           <div className='card' style={{estilocard}}>
-          <div className='row'>
-          
-          {imagenes.map(imagen=>(
-              <> 
-              <div className='col-md-4'>            
-              <img key={imagen.id} className='card-img-top' src={imagen.urls.small_s3} alt={imagen.likes} />                
-              <div className='card-body margen'>
-                  <h4 className='card-title'>{imagen.alt_description}</h4>
-                  <p className='card-text text-secondary'>
+            <div className='row' >
+                {imagenes.map(imagen=>(
+                    <> 
                     {
-                      imagen.description
+                      imagen.height<5000
                       ?
-                      imagen.description
+                      <div className='col-md-4'>            
+                        <img key={imagen.id} className='card-img-top' src={imagen.urls.small_s3} alt={imagen.likes} />                
+                          <div className='card-body margen'>
+                              <h4 className='card-title'>{imagen.alt_description}</h4>
+                              <p className='card-text text-secondary'>
+                                {
+                                  imagen.user.location
+                                  ?
+                                  imagen.user.location
+                                  :
+                                  "not location"
+                                }
+                              </p>
+                              <p className='card-text text-danger'>Likes: {imagen.likes}</p>
+                             {/* <p className='card-text text-danger'>Ancho: {imagen.width}</p>
+                              <p className='card-text text-danger'>Alto: {imagen.height}</p>  */}             
+                          </div>    
+                      </div>
                       :
-                      "not description"
+                      <>
+                      </>
                     }
-                  </p>
-                  <p className='card-text text-danger'>Likes: {imagen.likes}</p>
-                {/*   <a href="#" class="btn btn-primary">See Profile</a>  */}             
-              </div>    
+                    </>          
+                  ))
+                }
+                </div>
               </div>
-              </>          
-            ))
-          }         
-          </div>
-          </div>
+            </InfiniteScroll>            
       </div>        
     </div>
   )
